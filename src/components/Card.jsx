f8ee38c9a906fa17bc3a417e21f1393a3db65a12
kautsarabid <abidkautsar@gmail.com>
2024-04-23 10:46:54 +0700

    mengubah style code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,38 @@
 export default function Card(props) {
-	const {
-		variant2,
-		variant3,
-		title,
-		description1,
-		description2,
-		description3,
-		description4,
-		description5,
-		description6,
-		newStyle,
-		styleSpan,
-	} = props;
+	const { children, classCSSContainer } = props;
 
+	return <div className={`${classCSSContainer}`}>{children}</div>;
+}
+
+function Title(props) {
+	const { children, classCSSTitle } = props;
 	return (
-		<div className={`${variant2}`}>
-			<h5 className={`${variant3}`}>{title}</h5>
-			<p>{description1}</p>
-			<p>{description2}</p>
-			<p>{description3}</p>
-			<p>{description4}</p>
-			<p>{description5}</p>
-			<p>{description6}</p>
-			<span className={`${styleSpan}`}>{newStyle}</span>
-		</div>
+		<>
+			<h5 className={`${classCSSTitle}`}>{children}</h5>
+		</>
 	);
 }
+
+function Description(props) {
+	const { children } = props;
+
+	return (
+		<>
+			<p>{children}</p>
+		</>
+	);
+}
+
+function AdditionalStyle(props) {
+	const { children, classCSSAdd } = props;
+
+	return (
+		<>
+			<span className={classCSSAdd}>{children}</span>
+		</>
+	);
+}
+
+Card.Title = Title;
+Card.Description = Description;
+Card.AdditionalStyle = AdditionalStyle;
