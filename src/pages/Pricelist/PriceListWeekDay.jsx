2c73424b8e6a0947baaf460c7cf356498e6ebe05
kautsarabid <abidkautsar@gmail.com>
2024-04-23 11:33:52 +0700

    Mengubah style code

--- src/pages/Pricelist/PriceListWeekDay.jsx
+++ src/pages/Pricelist/PriceListWeekDay.jsx
@@ -3,42 +3,77 @@ import Card from "../../components/Card";
 export default function PriceListWeekDay() {
 	return (
 		<>
-			<h5 className="text-4xl uppercase text-center font-semibold bg-primary text-white py-4 mb-2 lg:hidden">
+			<h5 className="text-4xl uppercase text-center font-semibold bg-primary text-white py-4 mb-6 lg:hidden">
 				WeekDay
 			</h5>
-			<div className="flex justify-center items-center gap-10 flex-wrap  text-center mb-10">
-				<Card
-					variant2={"card-pricelist"}
-					variant3={"mb-4 font-bold text-5xl"}
-					title={"300K"}
-					description1={"06.00 - 07-00"}
-					description2={"07.00 - 08.00"}
-					description3={"09.00 - 10.00"}
-					description4={"09.00 - 10.00"}
-				/>
-				<Card
-					variant2={"card-pricelist"}
-					variant3={"mb-4 font-bold text-5xl"}
-					title={"250K"}
-					description1={"10.00 - 11.00"}
-					description2={"11.00 - 12.00"}
-					description3={"12.00 - 13.00"}
-					description4={"13.00 - 14.00"}
-					description5={"14.00 - 15.00"}
-					description6={"15.00 - 16.00"}
-				/>
-				<Card
-					variant2={"card-pricelist relative"}
-					variant3={"mb-4 font-bold text-5xl"}
-					title={"850K"}
-					description1={"16.00 - 18.00"}
-					description2={"20.00 - 22.00"}
-					description3={"22.00 - 24.00"}
-					newStyle={"⭐"}
-					styleSpan={
-						"absolute bg-yellow-400 -top-4 -right-4 border rounded-full p-2"
-					}
-				/>
+			<div className="flex justify-center items-center gap-10 flex-wrap text-center mb-10">
+				<Card classCSSContainer={"card-pricelist  relative"}>
+					<Card.Title classCSSTitle={"mb-4 font-bold text-5xl"}>
+						{" "}
+						300K{" "}
+					</Card.Title>
+					<Card.Description>06.00 - 07.00</Card.Description>
+					<Card.Description>07.00 - 08.00</Card.Description>
+					<Card.Description>08.00 - 09.00</Card.Description>
+					<Card.Description>09.00 - 10.00</Card.Description>
+
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute -top-3 font-bold border py-1 px-3 rounded-xl bg-primary text-white .inset-shadow-custom"
+						}
+					>
+						Morning
+					</Card.AdditionalStyle>
+				</Card>
+
+				<Card classCSSContainer={"card-pricelist relative"}>
+					<Card.Title classCSSTitle={"mb-4 font-bold text-5xl"}>
+						{" "}
+						250K{" "}
+					</Card.Title>
+
+					<Card.Description>10.00 - 11.00</Card.Description>
+					<Card.Description>11.00 - 12.00</Card.Description>
+					<Card.Description>12.00 - 13.00</Card.Description>
+					<Card.Description>13.00 - 14.00</Card.Description>
+					<Card.Description>14.00 - 15.00</Card.Description>
+					<Card.Description>15.00 - 16.00</Card.Description>
+
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute -top-3 font-bold border py-1 px-3 rounded-xl bg-primary text-white"
+						}
+					>
+						Noon
+					</Card.AdditionalStyle>
+				</Card>
+
+				<Card classCSSContainer={"card-pricelist relative"}>
+					<Card.Title classCSSTitle={"mb-4 font-bold text-5xl"}>
+						{" "}
+						850K{" "}
+					</Card.Title>
+
+					<Card.Description>16.00 - 18.00</Card.Description>
+					<Card.Description>20.00 - 22.00</Card.Description>
+					<Card.Description>22.00 - 24.00</Card.Description>
+
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute -top-3 font-bold border py-1 px-3 rounded-xl bg-primary text-white"
+						}
+					>
+						Night
+					</Card.AdditionalStyle>
+					{/* 
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute top-0 right-0 bg-yellow-400 px-3 py-1 rounded-md shadow-inner text-white font-bold"
+						}
+					>
+						Most Book
+					</Card.AdditionalStyle> */}
+				</Card>
 			</div>
 		</>
 	);
--- src/pages/Pricelist/PriceListWeekEnd.jsx
+++ src/pages/Pricelist/PriceListWeekEnd.jsx
@@ -3,32 +3,54 @@ import Card from "../../components/Card";
 export default function PriceListWeekEnd() {
 	return (
 		<>
-			<h5 className="text-4xl uppercase text-center font-semibold bg-secondary  text-white py-4 mb-2 lg:hidden">
+			<h5 className="text-4xl uppercase text-center font-semibold bg-secondary  text-white py-4 mb-6 lg:hidden">
 				Weekend
 			</h5>
+
 			<div className="flex flex-wrap justify-center items-center gap-10  text-center mb-10">
+				<Card classCSSContainer={"card-pricelist relative"}>
+					<Card.Title classCSSTitle={"mb-4 font-bold text-5xl"}>
+						{" "}
+						400K{" "}
+					</Card.Title>
+					<Card.Description>06.00 - 07.00</Card.Description>
+					<Card.Description>07.00 - 08.00</Card.Description>
+					<Card.Description>08.00 - 09.00</Card.Description>
+					<Card.Description>09.00 - 10.00</Card.Description>
+
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute -top-3 font-bold border py-1 px-3 rounded-xl bg-secondary text-white"
+						}
+					>
+						Morning
+					</Card.AdditionalStyle>
+				</Card>
+
+				<Card classCSSContainer={"card-pricelist relative"}>
+					<Card.Title classCSSTitle={"mb-4 font-bold text-5xl"}>
+						{" "}
+						300K{" "}
+					</Card.Title>
+
+					<Card.Description>10.00 - 11.00</Card.Description>
+					<Card.Description>11.00 - 12.00</Card.Description>
+					<Card.Description>12.00 - 13.00</Card.Description>
+					<Card.Description>13.00 - 14.00</Card.Description>
+					<Card.Description>14.00 - 15.00</Card.Description>
+					<Card.Description>15.00 - 16.00</Card.Description>
+
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute -top-3 font-bold border py-1 px-3 rounded-xl bg-secondary text-white"
+						}
+					>
+						Noon
+					</Card.AdditionalStyle>
+				</Card>
+
 				<Card
-					variant2={"card-pricelist"}
-					variant3={"mb-4 font-bold text-5xl"}
-					title={"400K"}
-					description1={"06.00 - 07-00"}
-					description2={"07.00 - 08.00"}
-					description3={"08.00 - 09.00"}
-					description4={"09.00 - 10.00"}
-				/>
-				<Card
-					variant2={"card-pricelist"}
-					variant3={"mb-4 font-bold text-5xl"}
-					title={"300K"}
-					description1={"10.00 - 11.00"}
-					description2={"11.00 - 12.00"}
-					description3={"12.00 - 13.00"}
-					description4={"13.00 - 14.00"}
-					description5={"14.00 - 15.00"}
-					description6={"15.00 - 16.00"}
-				/>
-				<Card
-					variant2={"card-pricelist relative"}
+					classCSSContainer={"card-pricelist relative"}
 					variant3={"mb-4 font-bold text-5xl"}
 					title={"900K"}
 					description1={"16.00 - 18.00"}
@@ -38,7 +60,32 @@ export default function PriceListWeekEnd() {
 					styleSpan={
 						"absolute bg-yellow-400 -top-4 -right-4 border rounded-full p-2"
 					}
-				/>
+				>
+					<Card.Title classCSSTitle={"mb-4 font-bold text-5xl"}>
+						{" "}
+						900K{" "}
+					</Card.Title>
+
+					<Card.Description>16.00 - 18.00</Card.Description>
+					<Card.Description>20.00 - 22.00</Card.Description>
+					<Card.Description>22.00 - 24.00</Card.Description>
+
+					<Card.AdditionalStyle
+						classCSSAdd={
+							"absolute -top-3 font-bold border py-1 px-3 rounded-xl bg-secondary text-white"
+						}
+					>
+						Night
+					</Card.AdditionalStyle>
+
+					{/* <Card.AdditionalStyle
+						classCSSAdd={
+							"absolute top-0 right-0 bg-yellow-400 px-3 py-1 rounded-md shadow-inner text-white font-bold"
+						}
+					>
+						Most Book
+					</Card.AdditionalStyle> */}
+				</Card>
 			</div>
 		</>
 	);
