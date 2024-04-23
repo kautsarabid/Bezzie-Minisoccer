185baa231882e3c6f30cd47dc51f8f76edc702c2
kautsarabid <abidkautsar@gmail.com>
2024-04-23 10:49:34 +0700

    mengubah style shadow

--- src/components/Navbar/NavListMobile.jsx
+++ src/components/Navbar/NavListMobile.jsx
@@ -3,7 +3,7 @@ export default function NavListMobile({ items, isOpen }) {
 		<div
 			className={
 				isOpen
-					? "md:hidden bg-secondary py-2 absolute w-3/6 rounded-md shadow-md top-24 right-10 transition duration-300 ease-in-out"
+					? "md:hidden bg-secondary py-2 absolute w-3/6 rounded-md shadow-custom top-24 right-10 transition duration-300 ease-in-out"
 					: "hidden"
 			}
 		>
