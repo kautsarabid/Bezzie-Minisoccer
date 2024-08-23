dfbc9877aaf44cd855b3b6ae6d518e06da175269
kautsarabid <abidkautsar@gmail.com>
2024-08-24 02:11:13 +0700

    Membuat file asset untuk icon

---
+++ src/components/AssetsIcons.jsx
@@ -0,0 +1,7 @@
+import Phone from "../assets/icons/Phone2.png";
+import People from "../assets/icons/People-Hugging.png";
+import Room from "../assets/icons/Room2.png";
+
+const Icon = { Phone, People, Room };
+
+export default Icon;
