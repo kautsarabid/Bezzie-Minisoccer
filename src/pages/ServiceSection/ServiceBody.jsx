4c2cedd934fec98c61cc34d79deb8d5a76907a31
kautsarabid <abidkautsar@gmail.com>
2024-09-02 01:02:23 +0700

    mengubah bahasa ke bahasa indonesia

--- src/pages/ServiceSection/ServiceBody.jsx
+++ src/pages/ServiceSection/ServiceBody.jsx
@@ -6,19 +6,19 @@ import Icon from "../../components/AssetsIcons";
 export default function ServiceBody() {
 	const itemsService = [
 		{
-			title: "Fast Respon",
+			title: "Cepat Tanggap",
 			description:
 				"Kami siap menanggapi setiap permintaan Anda dengan cepat dan efisien, memastikan Anda mendapatkan informasi dan layanan yang Anda butuhkan tanpa penundaan.",
 			img: Icon.Phone,
 		},
 		{
-			title: "Friendly",
+			title: "Ramah",
 			description:
 				"Tim kami selalu siap melayani Anda dengan senyuman dan keramahan, menciptakan suasana yang hangat dan menyenangkan setiap kali Anda berinteraksi dengan kami.",
 			img: Icon.People,
 		},
 		{
-			title: "Facility",
+			title: "Fasilitas",
 			description:
 				"Kami menawarkan fasilitas terbaik untuk mendukung setiap aktivitas Anda. Setiap detail dirancang untuk memberikan pengalaman terbaik dan membuat Anda merasa seperti di rumah sendiri.",
 			img: Icon.Room,
@@ -26,7 +26,7 @@ export default function ServiceBody() {
 	];
 	return (
 		<div id="service">
-			<h3 className="font-display text-center">Service</h3>
+			<h3 className="font-display text-center">Pelayanan</h3>
 
 			<ServiceList items={itemsService} />
 		</div>
